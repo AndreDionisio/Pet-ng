2fa675c31d27637c04061c757e960d6e82ba1669
AndreDionisio <andreluisdionisio@gmail.com>
2019-09-25 11:41:59 +0100

    first frontend

---
+++ src/app/hero.ts
@@ -0,0 +1,14 @@
+export class Hero {
+  id: number;
+  name: string;
+  age: number;
+  birthDate: string;
+  type: Type;
+  listVisits: Type[];
+}
+export class Type {
+  id: number;
+  name: string;
+
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 12, name: 'Narco' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 13, name: 'Bombasto' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 14, name: 'Celeritas' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 15, name: 'Magneta' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 16, name: 'RubberMan' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 17, name: 'Dynama' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 18, name: 'Dr IQ' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 19, name: 'Magma' , age: 1, birthDate: '', type:null, listVisits:[]},
+  { id: 20, name: 'Tornado' , age: 1, birthDate: '', type:null, listVisits:[]}
+];
